844183dcdce9e2c3da15282ce88019609ef1346b
Andrew Martin <andrew.thaddeus@gmail.com>
2019-01-13 16:34:07 -0500

    Deal with masking, exception handling, and forking in the stream sockets API

--- src/Socket/Stream.hs
+++ src/Socket/Stream.hs
@@ -1,10 +1,15 @@
 module Socket.Stream
   ( -- * Types
-    Acceptor
+    Listener
   , Connection
     -- * Establish
-  , listen
+  , listener
+  , withListener
   , accept
+  , accept_
+  , withAccepted
+  , forkAccepted
+  , forkAcceptedUnmasked
   , connect
   , disconnect
   , unsubscribe
@@ -13,17 +18,66 @@ module Socket.Stream
   , receive
   ) where
 
-newtype Acceptor = Acceptor Fd
+newtype Listener = Listener Fd
 newtype Connection = Connection Fd
 
-listen :: IO Acceptor
-accept :: Acceptor -> IO (Address,Connection)
-accept_ :: Acceptor -> IO Connection
+listener :: IO Listener
+withListener :: (Listener -> IO a) -> IO a
+withListener = bracket listener unsubcribe
+
+accept :: Listener -> IO (Address,Connection)
+accept_ :: Listener -> IO Connection
+
+-- | Accept a connection on the listener and run the supplied callback
+-- on it. This closes the connection when the callback finishes or if
+-- an exception is thrown. Since this function blocks the thread until
+-- the callback finishes, it is only suitable for stream socket clients
+-- that handle one connection at a time. The variant 'forkAcceptedUnmasked'
+-- is typically preferrable.
+withAccepted :: Listener -> (Connection -> Address -> IO a) -> IO a
+withAccepted x consume = do
+  internalWaitForConnection x
+  bracket
+    (internalAcceptConnection x)
+    (disconnect . snd)
+    (uncurry consume)
+
+-- | Accept a connection on the listener and run the supplied callback in
+-- a new thread. Prefer 'forkAcceptedUnmasked' unless the masking state
+-- needs to be preserved for the callback. Such a situation seems unlikely
+-- to the author.
+forkAccepted :: Listener -> (Connection -> Address -> IO a) -> IO ThreadId
+forkAccepted x consume = do
+  internalWaitForConnection x
+  mask $ \restore -> forkIO $ do
+    (address,conn) <- internalAcceptConnection x
+    onException (restore (consume address conn)) (disconnect conn)
+    disconnect conn
+
+-- | Accept a connection on the listener and run the supplied callback in
+-- a new thread. The masking state is set to @Unmasked@ when running the
+-- callback.
+forkAcceptedUnmasked :: Listener -> (Connection -> Address -> IO a) -> IO ThreadId
+forkAcceptedUnmasked x consume = do
+  internalWaitForConnection x
+  mask_ $ forkIOWithUnmask $ \unmask -> do
+    (address,conn) <- internalAcceptConnection x
+    onException (unmask (consume address conn)) (disconnect conn)
+    disconnect conn
+
+internalWaitForConnection :: Listener -> IO ()
+internalWaitForConnection _ = do
+  fail "write internalWaitForConnection"
+
+internalAcceptConnection :: Listener -> IO (Address,Connection)
+internalAcceptConnection _ = fail "write unsafeAcceptConnection"
+
 connect :: Address -> IO Connection
+withConnection :: (Connection -> IO a) -> IO a
 
 send :: Connection -> ByteArray -> IO ()
 receive :: Connection -> IO (Maybe ByteArray)
 
 disconnect :: Connection -> IO ()
-unsubcribe :: Acceptor -> IO ()
+unsubcribe :: Listener -> IO ()
 
